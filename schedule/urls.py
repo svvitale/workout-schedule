01cfdb6bc816c1f70360f69b6645f97c510dd04b
Scott Vitale <svvitale@gmail.com>
2016-12-19 22:22:48 -0700

    Add static URL mappings

--- schedule/urls.py
+++ schedule/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from .views import IndexView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
